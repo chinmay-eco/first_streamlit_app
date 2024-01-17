a0165706b9626a6abf3a9ce2d54c8bbdaaa16ac9
chinmay-eco <156855503+chinmay-eco@users.noreply.github.com>
2024-01-17 09:52:05 -0600

    Update streamlit_app.py
    normalizing json

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞Avocado Toast1')
+streamlit.text('🥑🍞Avocado Toast')
                
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
@@ -22,4 +22,6 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
